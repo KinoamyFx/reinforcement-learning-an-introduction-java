3189abf968d901ec7bc5add44f41e2740cc62a72
kinoamyfx <kinoamyfx@gmail.com>
2017-12-30 17:35:16 +0800

    draw chart
    Signed-off-by: kinoamyfx <kinoamyfx@gmail.com>

--- rlai-chapter02/src/test/java/rlai/GamblerTest.java
+++ rlai-chapter02/src/test/java/rlai/GamblerTest.java
@@ -1,15 +1,21 @@
 package rlai;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 
-import org.junit.Test;
+public class GamblerTest extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-public class GamblerTest {
-    @Test
-    public void testGambler() {
-        List<Bandit> bandits = new ArrayList<>(10);
+    @Override
+    public void start(Stage primaryStage) {
+        List<Bandit> bandits = new ArrayList<>();
         bandits.add(new GaussianBandit(20));
         bandits.add(new GaussianBandit(15));
         bandits.add(new GaussianBandit(10));
@@ -21,14 +27,22 @@ public class GamblerTest {
         bandits.add(new GaussianBandit(-20));
         bandits.add(new GaussianBandit(-25));
 
-        BanditStrategy strategy = new EpsilonGreedyStrategy(0.01, bandits.size());
-        Gambler gambler = new Gambler(bandits, strategy);
+        int iterateTimes = 3000;
+        Rlai2DChart chart = new Rlai2DChart("Epsilon-Greedy", "Iteration", "Average Reward");
 
-        double[] xRange = IntStream.range(0, 10000).asDoubleStream().toArray();
-
-        IntStream.range(0, 10000).forEach(i -> {
-            System.out.println(gambler.play());
+        DoubleStream.of(0.01, 0.05, 0.1, 0.4, 0.6, 0.8).forEach(d -> {
+            BanditStrategy strategy = new EpsilonGreedyStrategy(d, bandits.size());
+            Gambler gambler = new Gambler(bandits, strategy);
+            double[][] data = new double[2][iterateTimes];
+            data[0] = IntStream.range(0, iterateTimes).asDoubleStream().toArray();
+            data[1] = IntStream.range(0, iterateTimes).mapToDouble(i -> gambler.play()).toArray();
+            chart.addLine("ε=" + String.valueOf(d), data);
         });
-    }
 
+        primaryStage.setScene(chart.toScene());
+        primaryStage.setTitle("Epsilon-Greedy");
+        primaryStage.setWidth(1366);
+        primaryStage.setHeight(768);
+        primaryStage.show();
+    }
 }
